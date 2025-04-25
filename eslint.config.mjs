3c79f7ef397d678e3040fdc52d700304799c7df8
guodap <guoda.puidokaite@gmail.com>
2025-04-25 14:03:38 +0200

    fix: refactor to esmodule
    - Refactored eslint config to ESModule
    - Refactored jest config to ESModule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,26 @@ import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
+  js.configs.recommended,
   tseslint.configs.recommended,
+  { 
+    files: ["**/*.{js,mjs,cjs,ts}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"] 
+  },
+  { 
+    files: ["**/*.{js,mjs,cjs,ts}"], 
+    languageOptions: { 
+      globals: globals.browser 
+    } 
+  },
+  {
+    files: ["**/*.{test,spec}.{js,ts}"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      }
+    }
+  }
 ]);
